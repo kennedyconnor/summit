ecb8d7dcab80730729a1ae3113bea274c4cf589a
CourtneyHartman <courtneylarine@gmail.com>
2019-06-26 14:08:35 -0600

    trying to pull down

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,28 +1,27 @@
 <template>
-  <div class="row home">
-    <img class="parallax" alt="Summit Mountain" src="../assets/mountain2.png">
+  <div>
+    <div class="row home">
+      <img class="background" alt="Summit Mountain" src="../assets/mountain2.png">
 
-    <div class="col d-flex justify-content-end">
-      <div class="card" style="width: auto">
-        <div class="card-header" style="background-color: #346575">{{ day }}
+
+      <div class="col-10 d-flex justify-content-end">
+        <div class="card" style="width: auto">
+          <div class="card-header" style="background-color: #346575">{{ day }}
+          </div>
+          <ul class="list-group list-group-flush" v-for="task in instances" v-if="task.completed == false">
+            <label class="checkbox-inline task-font" v-bind:class="{isChecked: task.completed}"><input type="checkbox"
+                v-model="task.completed" @click="toggleTaskStatus(task, event)"
+                class="task-font">{{task.taskData.title}}
+              --
+              {{task.taskData.points}} points</li></label>
+          </ul>
         </div>
-        <ul class="list-group list-group-flush" v-for="task in instances" v-if="task.completed == false">
-          <label class="checkbox-inline task-font" v-bind:class="{isChecked: task.completed}"><input type="checkbox"
-              v-model="task.completed" @click="toggleTaskStatus(task, event)" class="task-font">{{task.taskData.title}}
-            --
-            {{task.taskData.points}} points</li></label>
-        </ul>
       </div>
     </div>
     <div class="row">
-      <!-- vertical progress bar -->
-      <div class="col d-flex justify-content-start">
-        <div class="progress" style="height: 20px;">
-          <div class="progress-bar bg-secondary" role="progressbar" style="width: 25%;" aria-valuenow="50"
-            aria-valuemin="0" aria-valuemax="2000"></div>
-        </div>
-      </div>
+      <ProgressBar />
     </div>
+
   </div>
 </template>
 
@@ -31,6 +30,7 @@
 
   import DayTasks from '@/components/DayTasks.vue';
   import TaskCalendar from '@/components/TaskCalendar.vue';
+  import ProgressBar from '@/components/ProgressBar.vue';
 
   export default {
     name: 'home',
@@ -63,7 +63,8 @@
     },
     components: {
       DayTasks,
-      TaskCalendar
+      TaskCalendar,
+      ProgressBar
     },
     methods: {
       logout() {
@@ -89,10 +90,10 @@
 </script>
 
 <style>
-  .parallax {
+  .background {
     background-size: cover;
     background-repeat: no-repeat;
-    height: 100vh;
+    height: 92vh;
     background-attachment: fixed;
     position: absolute;
   }
@@ -108,10 +109,11 @@
     height: 80vh;
   }
 
+  /* 
   .progress-bar {
     box-shadow: inset 0px 4px 6px rgba(100, 100, 100, 0.6);
     transform: rotate(-90deg);
-  }
+  } */
 
   .card-header {
     color: whitesmoke;
@@ -124,7 +126,5 @@
     font-family: 'Raleway', sans-serif;
     text-align: start;
     padding: 5px 5px 0px 5px;
-
-
   }
 </style>
