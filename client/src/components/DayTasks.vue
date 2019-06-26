47e517a6d8a3f83c6b8d6eeab0dbd30c49b2fb4a
Connor Kennedy <kennedyconnor02@gmail.com>
2019-06-26 12:02:08 -0600

    Task Calendar Styling In Progress

--- client/src/components/DayTasks.vue
+++ client/src/components/DayTasks.vue
@@ -2,11 +2,11 @@
   <div class="col-sm border">
     <h3 style="padding-top: 10px; padding-bottom: 0px;">{{day}}</h3>
     <hr style="padding: 0px;">
-    <div v-for="task in instances" :class="task.taskData.tags[0]">
-      <label class="checkbox" v-bind:class="{isChecked: task.completed}"><input type="checkbox" v-model="task.completed"
-          @click="toggleTaskStatus(task, $event)"> {{task.taskData.title}} -
-        {{task.taskData.points}} points <button v-if="task.taskData.completed = 'true'"
-          class="btn fas fa-trash-alt fa-sm" @click="deleteUserTaskInstance(task)"></button>
+    <div v-for="task in instances" class="text-nowrap d-flex" :class="task.taskData.tags[0]">
+      <label class="checkbox " v-bind:class="{isChecked: task.completed}"><input type="checkbox"
+          v-model="task.completed" @click="toggleTaskStatus(task, $event)"> {{task.taskData.title}}<button
+          v-if="task.taskData.completed = 'true'" class="btn fas fa-trash-alt fa-sm"
+          @click="deleteUserTaskInstance(task)"></button>
       </label>
       <br>
     </div>
